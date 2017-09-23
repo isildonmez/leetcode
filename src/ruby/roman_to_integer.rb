a146640b546b6466ef593707fe121e77de34ad4c
Isil Donmez <isil.donmez@gmail.com>
2017-09-24 00:30:33 +0100

    Correct some whitespaces and digits

--- src/ruby/roman_to_integer.rb
+++ src/ruby/roman_to_integer.rb
@@ -1,5 +1,5 @@
 def roman_to_int(s)
-  hash = {I: 1, V: 5, X: 10, L: 50,	C: 100,	D: 500,	M: 1000}
+  hash = {I: 1, V: 5, X: 10, L: 50, C: 100,	D: 500,	M: 1000}
   result = 0
 
   for i in 0...s.size - 1
@@ -18,7 +18,7 @@ end
 p roman_to_int("I")
 p roman_to_int("III")
 p roman_to_int("VII")
-p roman_to_int("IIX")
+p roman_to_int("IX")
 p roman_to_int("V")
 p roman_to_int("MCMLIV")
 p roman_to_int("MMXIV")
