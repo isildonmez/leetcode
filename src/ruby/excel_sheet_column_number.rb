d1fa1d67ad7d1ba06ea9fbd636eb886e1e909521
Isil Donmez <isil.donmez@gmail.com>
2017-12-10 18:48:50 +0000

    Delete the old version of the method

--- src/ruby/excel_sheet_column_number.rb
+++ src/ruby/excel_sheet_column_number.rb
@@ -1,9 +1,8 @@
 def title_to_number(s)
-  if s.size == 1
-    column_no = s.ord - 64
-  else
-    column_no = title_to_number(s[-1])
-    column_no += title_to_number(s[0]) * 26
+  column_no = 0
+  digit = s.length - 1
+  digit.downto(0) do |i|
+    column_no += (s[digit - i].ord - 64) * (26 ** i)
   end
   column_no
 end
@@ -12,13 +11,3 @@ puts title_to_number("A") # 1
 puts title_to_number("B") # 2
 puts title_to_number("Z") # 26
 puts title_to_number("AA") # 27
-
-
-def title_to_number(s)
-  column_no = 0
-  digit = s.length - 1
-  digit.downto(0) do |i|
-    column_no += (s[digit - i].ord - 64) * (26 ** i)
-  end
-  column_no
-end
