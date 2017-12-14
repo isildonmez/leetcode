99f00d3e33dbca637263ac4a80f4afe9ad668af3
Isil Donmez <isil.donmez@gmail.com>
2017-12-14 12:50:31 +0000

    Change the method

--- src/ruby/maximum_subarray.rb
+++ src/ruby/maximum_subarray.rb
@@ -1,24 +1,3 @@
-def max_sub_array(nums)
-  max = 0
-  subarray = nums
-  sum = 0
-
-  neg_idx = subarray.find_index{|num| num < 0 }
-  until neg_idx.nil?
-    sum += subarray[0...neg_idx].sum
-    max = sum if sum > max
-    sum += subarray[neg_idx]
-    sum = 0 if sum < 0
-    subarray = subarray[(neg_idx+1)..-1]
-    neg_idx = subarray.find_index{|num| num < 0 }
-  end
-  sum += subarray.sum
-  [sum, max].max
-end
-
-puts max_sub_array([-2,1,-3,4,-1,2])
-puts max_sub_array([-2,1,-3,4,-1,-1,2,1,-4,-2,6])
-
 def max_sub_array2(nums)
   max = 0
   sum = 0
@@ -27,9 +6,14 @@ def max_sub_array2(nums)
     sum = 0 if sum < 0
     max = sum if sum > max
   end
+  return nums.max if nums.select{|num| num > 0}.empty?
   max
 end
 
 puts max_sub_array2([-2,1,-3,4,-1,2])
-puts max_sub_array2([-2,1,-3,4,-1,-1,2,1,-4,-2,6])
+puts max_sub_array2([-2,1,-3,4,-1,-1,2,1,-4,-2])
+puts max_sub_array2([-4,-2])
 
+# 5
+# 5
+# -2
