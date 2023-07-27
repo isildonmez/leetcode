78cf6ac5dbbe165e24c7ba88128f3adfdefc9d48
isildonmez <isil.donmez@gmail.com>
2023-07-27 15:11:32 +0800

    Add test

--- src/python/max_avg_subarray1.py
+++ src/python/max_avg_subarray1.py
@@ -19,16 +19,10 @@ class Solution(object):
             if sub_sum > highest_sum:
                 highest_sum = sub_sum
         return highest_sum / k
-        
-
-            
-
-
-
-        for starting_idx in range(len(nums) - k + 1):
-            sum = 0
-            for idx in range(starting_idx, starting_idx + k - 1):
-                sum += nums[idx]
-            if sum > highest_sum:
-                highest_sum = sum
-        return highest_sum / k
+    
+if __name__ == "__main__":
+    print("Testing...")
+    s = Solution()
+    assert s.findMaxAverage([1,12,-5,-6,50,3], 4) == 12.75
+    assert s.findMaxAverage([5], 1) == 5.0
+    print("All passed")
