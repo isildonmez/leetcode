9f76a942c76f9d80c5c70965711ba42a2f15ba3a
isildonmez <isil.donmez@gmail.com>
2023-08-03 12:35:54 +0800

    number of recent calls

---
+++ src/python/number_of_recent_calls.py
@@ -0,0 +1,29 @@
+# from https://leetcode.com/problems/number-of-recent-calls/?envType=study-plan-v2&envId=leetcode-75
+
+from collections import deque
+
+
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = []
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        for idx, r in enumerate(self.requests):
+            if r >= t - 3000:
+                self.requests = self.requests[idx:]
+                break
+        return len(self.requests)
+    
+
+class AlternativeRecentCounter:
+
+    def __init__(self):
+        self.requests = deque()
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        while self.requests[0] < t - 3000:
+            self.requests.popleft()
+        return len(self.requests)
