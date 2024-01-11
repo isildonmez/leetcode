83314fa6860bb03613b967237ffd9891607a7e92
isildonmez <isil.donmez@gmail.com>
2024-01-11 19:09:48 +1100

    Add solution to Summary Ranges

---
+++ src/python/summary_ranges.py
@@ -0,0 +1,24 @@
+class Solution:
+    def summary_ranges(self, nums: list[int]) -> list[str]:
+        result = []
+        idx = 0
+        while idx < len(nums):
+            start, end = idx, idx
+            while idx + 1 < len(nums) and nums[idx] == nums[idx + 1] - 1:
+                end = idx + 1
+                idx += 1
+            if start == end:
+                result.append(f"{nums[start]}")
+            else:
+                result.append(f"{nums[start]}->{nums[end]}")
+            idx += 1
+        return result
+            
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print("Testing...")
+    assert s.summary_ranges([0,1,2,4,5,7]) == ["0->2","4->5","7"]
+    # assert s.summary_ranges([0,2,3,4,6,8,9]) == ["0","2->4","6","8->9"]
+    print("Done!")
