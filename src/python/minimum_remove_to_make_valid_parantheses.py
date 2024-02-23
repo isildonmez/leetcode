26425e3a1d09a59fc1c4336b9af65b7ab1c49fd9
isildonmez <isil.donmez@gmail.com>
2024-02-24 00:25:43 +1300

    Add solution to minimum remove to make valid parantheses

---
+++ src/python/minimum_remove_to_make_valid_parantheses.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        left = deque()
+        right = deque()
+        for i, c in enumerate(s):
+            if c == "(":
+                left.append(i)
+            if c == ")":
+                if len(left) > 0:
+                    left.pop()
+                else:
+                    right.append(i)
+        to_remove = set(left + right)
+        result = []
+        for i, c in enumerate(s):
+            if i not in to_remove:
+                result.append(c)
+        return "".join(result)
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print("Testing...")
+    assert s.minRemoveToMakeValid("a)b(c)d") == "ab(c)d"
+    assert s.minRemoveToMakeValid("))((") == ""
+    print("Done!")
