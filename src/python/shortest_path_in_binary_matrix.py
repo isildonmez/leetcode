d7543e05625465d0fb4f34fbd0d8d5e9d68928bf
isildonmez <isil.donmez@gmail.com>
2024-03-01 11:22:00 +1300

    Add solution to shortest_path_in_binary_matrix

---
+++ src/python/shortest_path_in_binary_matrix.py
@@ -0,0 +1,41 @@
+from collections import deque
+from typing import Optional
+
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: list[list[int]]) -> int:
+        n = len(grid)
+        if grid[0][0] != 0: return -1
+        if grid[n-1][n-1] != 0: return -1
+        heads = deque()
+        # (step_count, x, y)
+        heads.append((1,0,0))
+        grid[0][0] = 1
+        while len(heads) != 0:
+            step_count, x, y = heads.popleft()
+            if x == n-1 and y == n-1:
+                return step_count
+            for i in range(x-1, x+2):
+                for j in range(y-1, y+2):
+                    if i < 0 or i > n-1: continue
+                    if j < 0 or j > n-1: continue
+                    if grid[i][j] != 0: continue
+                    heads.append((step_count + 1, i, j))
+                    grid[i][j] = 1
+        return -1
+
+    
+if __name__ == "__main__":
+    s = Solution()
+    print("Testing...")
+    assert s.shortestPathBinaryMatrix([[0,1],[1,0]]) == 2
+    assert s.shortestPathBinaryMatrix([[0,0,0],[1,1,0],[1,1,0]]) == 4
+    assert s.shortestPathBinaryMatrix([[1,0,0],[1,1,0],[1,1,0]]) == -1
+    assert s.shortestPathBinaryMatrix([
+        [0,1,0,0,0,0],
+        [0,1,1,1,1,1],
+        [0,0,0,0,1,1],
+        [0,1,0,0,0,1],
+        [1,0,0,1,0,1],
+        [0,0,1,0,1,0]]
+    ) == 7
+    print("Done!")
