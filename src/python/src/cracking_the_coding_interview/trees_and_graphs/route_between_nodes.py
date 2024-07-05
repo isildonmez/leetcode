f9fae7dc19f337f78c0e77147e0f50af8c08656f
isildonmez <isil.donmez@gmail.com>
2024-07-05 13:20:04 +0200

    Fix route_between_nodes solution

--- src/python/src/cracking_the_coding_interview/trees_and_graphs/route_between_nodes.py
+++ src/python/src/cracking_the_coding_interview/trees_and_graphs/route_between_nodes.py
@@ -1,31 +1,47 @@
+from collections import deque
 from typing import Optional
 
 
-class Node:
-    def __init__(
-        self, next: Optional["Node"] = None, visited: Optional[int] = False
-    ) -> None:
-        self.visited = visited
-        self.next = next
+class GNode:
+    def __init__(self, val: int, children: Optional[list["GNode"]] = None) -> None:
+        self.val = val
+        self.children = children
 
 
-def route_between_nodes(head: Node) -> bool:
-    while head is not None:
-        if head.visited == True:
-            return True
-        head.visited = True
-        head = head.next
+def route_between_nodes(node1: GNode, node2: GNode) -> bool:
+    if node1 == node2:
+        return True
+    children1, children2 = deque([node1]), deque([node2])
+    paths1, paths2 = set(), set()
+    while len(children1) != 0 or len(children2) != 0:
+        if len(children1) != 0:
+            curr1 = children1.popleft()
+            paths1.add(curr1)
+            if node2 in paths1:
+                return True
+            if curr1.children is not None:
+                for c in curr1.children:
+                    children1.append(c)
+        if len(children2) != 0:
+            curr2 = children2.popleft()
+            paths2.add(curr2)
+            if node1 in paths2:
+                return True
+            if curr2.children is not None:
+                for c in curr2.children:
+                    children2.append(c)
     return False
 
 
 if __name__ == "__main__":
     print("Testing...")
-    # n1 -> n2 -> n3 -> n1
-    n1, n2, n3 = Node(), Node(), Node()
-    n1.next = n2
-    n2.next = n3
-    n3.next = n1
-    assert route_between_nodes(n1) == True
-    n4 = Node(Node(Node()))
-    assert route_between_nodes(n4) == False
+    # n1 -> n2 -> n3
+    # n4 -> n5 -> n3
+    n1, n2, n3, n4, n5 = GNode(1), GNode(2), GNode(3), GNode(4), GNode(5)
+    n1.children = [n2]
+    n2.children = [n3]
+    n4.children = [n5]
+    n5.children = [n2]
+    assert route_between_nodes(n1, n3) == True
+    assert route_between_nodes(n1, n4) == False
     print("Done!")
