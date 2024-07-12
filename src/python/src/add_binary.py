73e0850a75d90682c307d44f4b0e86d03b1e2789
isildonmez <isil.donmez@gmail.com>
2024-07-12 17:03:30 +0100

    Format

--- src/python/src/add_binary.py
+++ src/python/src/add_binary.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/add-binary/description/?envType=study-plan-v2&envId=top-interview-150
 
+
 class Solution:
     def add_binary(self, a: str, b: str) -> str:
         total = int(a, 2) + int(b, 2)
--- src/python/src/plus_one.py
+++ src/python/src/plus_one.py
@@ -1,10 +1,11 @@
 # https://leetcode.com/problems/plus-one/description/?envType=study-plan-v2&envId=top-interview-150
 
+
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         remainder = 1
         newDigits = digits.copy()
-        for i in range(len(digits)-1, -1, -1):
+        for i in range(len(digits) - 1, -1, -1):
             total = newDigits[i] + remainder
             remainder = total // 10
             newDigits[i] = total % 10
