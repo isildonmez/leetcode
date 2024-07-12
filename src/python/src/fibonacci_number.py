a6ef9ccdb2d178debdc630abe645394ee0f485f6
isildonmez <isil.donmez@gmail.com>
2024-07-12 17:03:22 +0100

    Add solution to fibonacci

---
+++ src/python/src/fibonacci_number.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/fibonacci-number/description/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        left, right = 0, 1
+        for i in range(2, n + 1):
+            left, right = right, left + right
+        return right
