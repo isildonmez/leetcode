d6dc57d088c6e374fe811d89f73a8ea7436ee813
isildonmez <isil.donmez@gmail.com>
2024-07-12 15:18:47 +0100

    Add solution to add_binary

---
+++ src/python/src/add_binary.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/add-binary/description/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def add_binary(self, a: str, b: str) -> str:
+        total = int(a, 2) + int(b, 2)
+        result = ""
+        power = 0
+        while total >= 2:
+            total, current_digit = total // 2, total % 2
+            result = f"{current_digit}" + result
+        return f"{total}" + result
+
+    def alternative_add_binary(self, a: str, b: str) -> str:
+        return bin(int(a, 2) + int(b, 2))[2:]
