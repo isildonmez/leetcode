460bc4710cecb9aceb664c28f70a320e29ed23f5
isildonmez <isil.donmez@gmail.com>
2024-07-26 14:09:30 +0200

    Add reverse_bits solution

---
+++ src/python/src/reverse_bits.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        b = bin(n)[2:].zfill(32)
+        r = b[::-1]
+        return int(r, 2)
+
+
+if __name__ == "__main__":
+    print("Testing...")
+    s = Solution()
+    s.reverseBits(43261596) == 964176192
+    s.reverseBits(4294967293) == 3221225471
+    print("Done!")
