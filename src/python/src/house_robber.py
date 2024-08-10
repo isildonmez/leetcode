fc48a5df56424da3b0008bb78b022258fb2658a2
isildonmez <isil.donmez@gmail.com>
2024-08-10 18:17:47 +0100

    Fix house_robber

--- src/python/src/house_robber.py
+++ src/python/src/house_robber.py
@@ -3,14 +3,14 @@
 
 class Solution:
     def rob(self, nums: list[int]) -> int:
-        evens = 0
-        odds = 0
-        for i in range(len(nums)):
-            if i % 2 == 0:
-                evens += nums[i]
-            else:
-                odds += nums[i]
-        return max(evens, odds)
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        m = [0] * n
+        m[0], m[1] = nums[0], max(nums[1], nums[0])
+        for i in range(2, len(nums)):
+            m[i] = max(m[i - 1], m[i - 2] + nums[i])
+        return m[-1]
 
 
 if __name__ == "__main__":
@@ -18,4 +18,5 @@ if __name__ == "__main__":
     print("Testing...")
     assert s.rob([1, 2, 3, 1]) == 4
     assert s.rob([2, 7, 9, 3, 1]) == 12
+    assert s.rob([2, 1, 1, 2]) == 4
     print("Done!")
