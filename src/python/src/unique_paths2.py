2c93e4d5260134a30d56ea464b58103c2f0a75e0
isildonmez <isil.donmez@gmail.com>
2024-08-10 18:36:37 +0100

    Fix unique_paths2

--- src/python/src/unique_paths2.py
+++ src/python/src/unique_paths2.py
@@ -1,31 +1,26 @@
 # https://leetcode.com/problems/unique-paths-ii/?envType=study-plan-v2&envId=top-interview-150
 
 
-from collections import deque
-
-
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
-        num_of_paths = 0
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        if n == 0:
-            return 0
-        locations = deque()
-        locations.append((0, 0))
-        while len(locations) > 0:
-            i, j = locations.popleft()
-            if (i == m - 1 and j == n - 2) or (i == m - 2 and j == n - 1):
-                num_of_paths += 1
-                continue
-            if j + 1 < n and obstacleGrid[i][j + 1] == 0:
-                locations.append((i, j + 1))
-            if i + 1 < m and obstacleGrid[i + 1][j] == 0:
-                locations.append((i + 1, j))
-        return num_of_paths
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        if obstacleGrid[0][0] == 0:
+            dp[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    continue
+                if i > 0:
+                    dp[i][j] += dp[i - 1][j]
+                if j > 0:
+                    dp[i][j] += dp[i][j - 1]
+        return dp[m - 1][n - 1]
 
 
 if __name__ == "__main__":
     s = Solution()
     print("Testing...")
     assert s.uniquePathsWithObstacles([[0, 0, 0], [0, 1, 0], [0, 0, 0]]) == 2
+    assert s.uniquePathsWithObstacles([[0]]) == 1
     print("Done!")
