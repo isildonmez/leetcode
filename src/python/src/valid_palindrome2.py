745eed35626c9cfca33598f4f883eaf3367392ca
isildonmez <isil.donmez@gmail.com>
2024-09-14 20:17:31 +0100

    Refactor valid_palindrome2 solution

--- src/python/src/valid_palindrome2.py
+++ src/python/src/valid_palindrome2.py
@@ -1,25 +1,17 @@
-# TODO: Check for better solutions in leetcode
+# https://leetcode.com/problems/valid-palindrome-ii/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 
 
 class Solution:
-    def is_palindrome(self, s: str) -> tuple[bool, int, int]:
+    def validPalindrome(self, s: str) -> bool:
         i, j = 0, len(s) - 1
-        if i == j:
-            return (True, i, j)
         while i < j:
             if s[i] != s[j]:
-                return (False, i, j)
+                sub1 = s[i:j]
+                sub2 = s[i + 1 : j + 1]
+                return sub1 == sub1[::-1] or sub2 == sub2[::-1]
             i += 1
             j -= 1
-        return (True, i, j)
-
-    def validPalindrome(self, s: str) -> bool:
-        p0, left, right = self.is_palindrome(s)
-        if p0:
-            return True
-        p1, _, _ = self.is_palindrome(s[left + 1 : right + 1])
-        p2, _, _ = self.is_palindrome(s[left:right])
-        return p1 or p2
+        return True
 
 
 if __name__ == "__main__":
