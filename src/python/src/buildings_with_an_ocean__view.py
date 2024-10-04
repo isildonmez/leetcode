15e392f44006f28af952bdb1e92d8c885b68edcd
isildonmez <isil.donmez@gmail.com>
2024-10-04 19:16:28 +0300

    Add solution to buildings_with_an_ocean_view

---
+++ src/python/src/buildings_with_an_ocean__view.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/buildings-with-an-ocean-view/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+
+
+class Solution:
+    def findBuildings(self, heights: list[int]) -> list[int]:
+        if len(heights) == 0: 
+            return[]
+        ocean_views = [len(heights)-1]
+        max_height = heights[-1]
+        for i in range(len(heights)-2, -1, -1):
+            if heights[i] > max_height:
+                ocean_views.append(i)
+                max_height = heights[i]
+        return ocean_views[::-1]
