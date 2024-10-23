daa373c3049f3f39a2eaed8207d9365819aee7b9
isildonmez <isil.donmez@gmail.com>
2024-10-23 16:59:52 +0100

    Refactor minimum_add_to_make_parenthesis_valid

--- src/python/src/minimum_add_to_make_parenthesis_valid.py
+++ src/python/src/minimum_add_to_make_parenthesis_valid.py
@@ -5,12 +5,11 @@ class Solution:
     def minAddToMakeValid(self, s: str) -> int:
         openings = 0
         moves = 0
-        for i, p in enumerate(s):
-            if p == "(":
+        for c in s:
+            if c == "(":
                 openings += 1
-                continue
-            if openings > 0:
+            elif c == ")" and openings > 0:
                 openings -= 1
-            else:
+            elif c == ")" and openings == 0:
                 moves += 1
         return moves + openings
