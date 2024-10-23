69ff80625fa927053f1d85100d23a55ab4a58e29
isildonmez <isil.donmez@gmail.com>
2024-10-23 16:51:27 +0100

    Refactor top_k_frequent_elements

--- src/python/src/top_k_frequent_elements.py
+++ src/python/src/top_k_frequent_elements.py
@@ -1,17 +1,34 @@
 # https://leetcode.com/problems/top-k-frequent-elements/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 
-from collections import Counter
-import heapq
+from collections import defaultdict
 
 
 class Solution:
+    # On
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        frequencies = Counter(nums)
-        freq_heap = []
-        for n, f in frequencies.items():
-            if len(freq_heap) < k:
-                heapq.heappush(freq_heap, (f, n))
-            elif f > freq_heap[0][0]:
-                heapq.heappop(freq_heap)
-                heapq.heappush(freq_heap, (f, n))
-        return [n for f, n in freq_heap]
+        frequencies_by_nums = defaultdict(int)
+        for n in nums:
+            frequencies_by_nums[n] += 1
+        min_f = float("inf")
+        max_f = -float("inf")
+        frequencies_to_nums = defaultdict(list)
+        for n, f in frequencies_by_nums.items():
+            frequencies_to_nums[f].append(n)
+            min_f = min(min_f, f)
+            max_f = max(max_f, f)
+        res = []
+        for f in range(max_f, min_f - 1, -1):
+            if f in frequencies_to_nums:
+                res.extend(frequencies_to_nums[f])
+            if len(res) == k:
+                return res
+
+    # # Onlogk
+    # def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+    #     dic=Counter(nums)
+    #     res=[]
+    #     for key,ct in dic.items():
+    #         heapq.heappush(res,[ct,key])
+    #         if len(res)>k:
+    #             heapq.heappop(res)
+    #     return [x[1] for x in res]
