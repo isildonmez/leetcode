b1617e2cef85d6daaaab5cbaccfa7ffe4036d4eb
isildonmez <isil.donmez@gmail.com>
2024-10-26 09:47:13 +0100

    Refactor group_shifted_strings

--- src/python/src/group_shifted_strings.py
+++ src/python/src/group_shifted_strings.py
@@ -4,18 +4,15 @@ from collections import defaultdict
 class Solution:
     def groupStrings(self, strings: list[str]) -> list[list[str]]:
         pattern_to_strings = defaultdict(list)
-        normaliser = ord("a")
         mod = ord("z") - ord("a") + 1
         for s in strings:
             pattern = ""
-            difference = ord(s[0]) - normaliser
+            normaliser = ord(s[0])
             for c in s:
-                ascii_num = ord(c) - difference
-                if ascii_num < ord("a"):
-                    ascii_num += mod
-                pattern += chr(ascii_num)
+                curr = (ord(c) - normaliser) % mod
+                pattern += chr(curr)
             pattern_to_strings[pattern].append(s)
-        return pattern_to_strings.values()
+        return list(pattern_to_strings.values())
 
 
 if __name__ == "__main__":
