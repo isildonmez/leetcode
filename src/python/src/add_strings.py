9382cbbd9e6c63ba99828881481b3e1a932e8c90
isildonmez <isil.donmez@gmail.com>
2024-10-27 13:07:42 +0000

    Add solution to add_strings

--- src/python/src/add_strings.py
+++ src/python/src/add_strings.py
@@ -1,40 +1,19 @@
-from collections import deque
+# https://leetcode.com/problems/add-strings/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
 
 
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        str_to_digit = {
-            "0": 0,
-            "1": 1,
-            "2": 2,
-            "3": 3,
-            "4": 4,
-            "5": 5,
-            "6": 6,
-            "7": 7,
-            "8": 8,
-            "9": 9,
-        }
+        res = ""
         carry = 0
-        digits = deque()
-        stop_idx = min(len(num1), len(num2))
-        for i in range(1, stop_idx + 1):
-            d1, d2 = str_to_digit[num1[-i]], str_to_digit[num2[-i]]
-            carry += d1 + d2
-            digit = carry % 10
-            carry //= 10
-            digits.appendleft(f"{digit}")
-        while carry != 0:
-            current_idx = len(digits) * -1 - 1
-            if len(num1) >= current_idx * -1:
-                carry += str_to_digit[num1[current_idx]]
-            if len(num2) >= current_idx * -1:
-                carry += str_to_digit[num2[current_idx]]
-            digit = carry % 10
-            carry //= 10
-            digits.appendleft(f"{digit}")
-        until = len(digits) * -1
-        return num1[:until] + num2[:until] + "".join(list(digits))
+        i, j = len(num1) - 1, len(num2) - 1
+        while i >= 0 or j >= 0 or carry:
+            n1 = int(num1[i]) if i >= 0 else 0
+            n2 = int(num2[j]) if j >= 0 else 0
+            carry, total = divmod(n1 + n2 + carry, 10)
+            res += str(total)
+            i -= 1
+            j -= 1
+        return res[::-1]
 
 
 if __name__ == "__main__":
