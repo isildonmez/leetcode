0f972c9a661c3995c1edcfebf05f7d6f688ac0d1
isildonmez <isil.donmez@gmail.com>
2024-12-18 10:15:15 +0000

    Refactor the solution of max_subarray

--- src/python/src/maximum_subarray.py
+++ src/python/src/maximum_subarray.py
@@ -3,12 +3,8 @@
 
 class Solution:
     def maxSubArray(self, nums: list[int]) -> int:
-        current_sum = max_sum = nums[0]
-        for i in range(1, len(nums)):
-            n = nums[i]
-            if current_sum <= 0:
-                current_sum = n
-            else:
-                current_sum += n
-            max_sum = max(current_sum, max_sum)
+        max_sum = current_sum = nums[0]
+        for num in nums[1:]:
+            current_sum = max(current_sum + num, num)
+            max_sum = max(max_sum, current_sum)
         return max_sum
